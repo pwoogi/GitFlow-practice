ab12b95ea0e99ed85baf4103ea774cb99353fb8e
pwoogi <chrishw1029@gmail.com>
2022-12-19 22:35:02 +0900

    git_practice & repeat

---
+++ src/test/java/com/practice/gitprac/GitPracApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.gitprac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitPracApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
