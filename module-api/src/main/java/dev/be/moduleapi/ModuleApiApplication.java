b2821c33c17359750f3fc2a46c3233aad8ef18a6
pwoogi <chrishw1029@gmail.com>
2022-12-20 17:55:27 +0900

    scanBasePackages로 path경로 설정

--- module-api/src/main/java/dev/be/moduleapi/ModuleApiApplication.java
+++ module-api/src/main/java/dev/be/moduleapi/ModuleApiApplication.java
@@ -1,9 +1,12 @@
-package dev.be; //package root 주의! 하위 목록들이 호환될 수 있도록 @ComponentScan 개념을 명시할 것!
+package dev.be.moduleapi; //package root 주의! 하위 목록들이 호환될 수 있도록 @ComponentScan 개념을 명시할 것!
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(
+        scanBasePackages = { "dev.be.moduleapi", "dev.be.modulecommon" }
+
+)
 public class ModuleApiApplication {
 
     public static void main(String[] args) {
